5d28ec8a149dd1f873c3b345636f51f5e1219830
pablonuñez10 <nunezbenitezpablo@gmail.com>
2024-03-12 12:22:30 -0300

    Fix git

--- src/TodoForm/index.js
+++ src/TodoForm/index.js
@@ -24,8 +24,10 @@ function TodoForm() {
             <label> Escribe tu nuevo TODO</label>
             <textarea placeholder="Cortar cebolla para el almuerzo" value={newTodoValue} onChange={onChange}/>
             <div className="TodoForm-buttonContainer">
-                <button type="button" className="TodoForm-button TodoForm-button--cancel" onClick={onCancel} >Cancelar</button>
-                <button type="submit" className="TodoForm-button TodoForm-button--add">Agregar</button>
+                <button type="button" className="TodoForm-button TodoForm-button--cancel"
+                onClick={onCancel} >Cancelar</button>
+                <button type="submit" className="TodoForm-button TodoForm-button--add">
+                Agregar</button>
             </div>
         </form>
     )
--- src/TodoSearch/index.js
+++ src/TodoSearch/index.js
@@ -1,20 +1,42 @@
 import React from 'react';
 import './TodoSearch.css'
 import { TodoContext } from '../TodoContext';
+// import { TodoContext } from '../TodoContext';
+
 function TodoSearch(){
-  const {
-    searchValue,
-    setSearchValue
-   } = React.useContext(TodoContext);
-   
-    return (
-     <input placeholder="Cortar Cebolla" className="TodoSearch" value={searchValue}
-     onChange={(event)=> {
-      setSearchValue(event.target.value);
+   // const {
+
+  //   searchValue,
+  //   setSearchValue,
+  //  } = React.useContext(TodoContext);''
+  // const [newAddTodo, setnewAddTodo] = React.useState ('')
+  // const onSubmit = (event) => {
+  //     event.preventDefault()
+  // newAddTodo();
+  // };
+  //  return (
+  //     <form onSubmit={onSubmit}>
+  //       <input placeholder='' onSubmit={onSubmit} value={setnewAddTodo}/>    
+  //     </form>
+  //        //  <input placeholder="Cortar Cebolla" className="TodoSearch" value={searchValue}
+  //  onChange={ (event)=> {
+  //   setSearchValue(event.target.value);
+  // }}
+  //  />  
+const [mostrarTodo] = React.useContext (TodoContext)
+const [ newAddTodo, setNewAddTodo] = React.useState ('')
+const handleChange = (event) => {
+  setNewAddTodo(event.target.value)
+  mostrarTodo(newAddTodo);
+};
+return (
+    <form>
+      <input type = 'text' value={newAddTodo}  onChange={handleChange} />
+      {/* <button onClick={NewAddTodo}> </button> */}
+    </form>
   
-     }}
-     />
-    );
+  );
+
   }
 
 export {TodoSearch};
